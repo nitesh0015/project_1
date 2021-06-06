66d202d0b51d35b9dce80d1d4c7bdfb6c96ff9b2
nitesh0015 <63290772+nitesh0015@users.noreply.github.com>
2021-06-06 11:32:17 +0530

    Add files via upload

---
+++ modern project/dark.js
@@ -0,0 +1,7 @@
+document.querySelector('.dark').addEventListener('click', function () {
+  document.querySelector('*').style.backgroundColor = '#121212';
+  document.querySelector('p').style.fontcolor = 'rgb(255, 255, 255)';
+  document.querySelector('button').style.backgroundColor = 'rgb(255, 255, 255)';
+  document.querySelector('button').style.fontcolor = '#bbe1fa';
+  document.querySelector('legend').style.fontcolor = 'rgb(255, 255, 255)';
+});
